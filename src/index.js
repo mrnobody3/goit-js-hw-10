36374eda2988425413fb5f01f0394914be304501
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2022-04-28 17:16:58 +0300

    commend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector('#search-box');
-searchBox.addEventListener('input', onSearchByName);
+searchBox.addEventListener('input', debounce(onSearchByName), 1000);
 
-function onSearchByName() {
+function onSearchByName(e) {
+  console.log(e);
   return fetch('https://restcountries.com/v2/all').then(r => console.log(r.json()));
 }
 onSearchByName().then(r => console.log(r));
