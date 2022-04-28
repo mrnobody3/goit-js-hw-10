1a0a349a65d02883800198c3583592f0a37b2a60
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2022-04-28 16:16:46 +0300

    json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+searchBox.addEventListener('input', onSearchByName);
+
+function onSearchByName() {
+  return fetch('https://restcountries.com/v2/all').then(r => console.log(r.json()));
+}
+onSearchByName().then(r => console.log(r));
