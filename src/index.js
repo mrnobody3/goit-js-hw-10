a84ea47a0f663b185f201c5321a3af4ecc21cc61
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2022-05-01 22:11:38 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,41 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
 const DEBOUNCE_DELAY = 300;
-// console.log(debounce);
 
 const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-lis');
 const countryInfo = document.querySelector('.country-info');
 searchBox.addEventListener('input', onSearchByName);
 
-function onSearchByName(e) {
-  console.log(e.currentTarget.value);
+function markupList(arr) {
+  return arr
+    .map(({ flags, name }) => {
+      return `<li class="country-item">
+        <img
+          class="country-img"
+          src="${flags.svg}"
+          alt="${name.official}"
+          width="25"
+          height="20"
+        />
+        <span>${name.official}</span>
+      </li>`;
+    })
+    .join('');
 }
-console.log(fetchCountries('peru').then(r => console.log(r)));
+function onSearchByName(e) {
+  let value = e.currentTarget.value.trim();
+  fetchCountries(value)
+    .then(r => {
+      return markupList(r);
+    })
+    .then(r => {
+      console.log(typeof r);
+      return renderMarkup(r);
+    })
+    .catch(error => console.error(error));
+}
+
+function renderMarkup(str = '') {
+  countryList.innerHTML = str;
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,11 @@
 function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(r => r.json());
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages,`,
+  ).then(r => {
+    if (!r.ok) {
+      throw new Error(r.status);
+    }
+    return r.json();
+  });
 }
-
 export { fetchCountries };
